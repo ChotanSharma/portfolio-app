efee9cb271349fc3245f0c6505f500fb10da0847
ChotanSharma <chotansharma@yahoo.com>
2022-04-23 12:51:27 -0400

    add user input validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
+    const [errorMessage, setErrorMessage] = useState('');
     
         const [formState, setFormState] = useState({ name: '', email: '', message: '' });
         const { name, email, message } = formState;
         function handleChange(e) {
-            setFormState({...formState, [e.target.name]: e.target.value })
+            if (e.target.name === 'email') {
+                const isValid = validateEmail(e.target.value);
+                console.log(isValid);
+                // isValid conditional statement
+                if (!isValid) {
+                    setErrorMessage('Your email is invalid.');
+                  } else {
+                    setErrorMessage('');
+                  } 
+              } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                  } else {
+                    setErrorMessage('');
+                  }
+              } 
+            console.log('errorMessage', errorMessage);
+            if (!errorMessage) {
+                setFormState({ ...formState, [e.target.name]: e.target.value });
+            }
         }
       
       console.log(formState);
